ef63fc0ed5e27abb427fb5eb5ed49c428497c209
If LIn <if.lin87048@gmail.com>
2018-01-21 18:08:38 +0800

    網狀圖版面長寬RWD設定完成。上傳單獨的網狀圖網頁。

--- js/net.js
+++ js/net.js
@@ -2,14 +2,20 @@
 
 
     var svg = d3.select("#people_net"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height"),
+        //        width = svg.attr("width"),
+        //        height = svg.attr("height"),
+
+        width = svg_bar.node().getBoundingClientRect().width - margin.left - margin.right,
+
+        height = svg_bar.node().getBoundingClientRect().height - margin.top - margin.bottom,
         node,
         link;
 
 
     var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
+        .force("link", d3.forceLink().id(function (d) {
+            return d.id;
+        }).distance(100).strength(1))
         .force("charge", d3.forceManyBody())
         .force("center", d3.forceCenter(width / 2, height / 2));
 
@@ -25,10 +31,10 @@
             .enter()
             .append("line")
             .attr("class", "link")
-        .style("stroke-width", function(d) {
+            .style("stroke-width", function (d) {
                 return d.value;
             }) //連結線的寬度//
-        .style("stroke","#a5a5a5") //連結線的顏色//
+            .style("stroke", "#a5a5a5") //連結線的顏色//
 
 
         node = svg.selectAll(".node")
@@ -37,26 +43,32 @@
             .append("g")
             .attr("class", "node")
             .call(d3.drag()
-                    .on("start", dragstarted)
-                    .on("drag", dragged)
-//                    .on("end", dragended)
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                //                    .on("end", dragended)
             );
 
         node.append("circle")
-            .attr("r", function(d) {
-                return (d.N_report/2);
+            .attr("r", function (d) {
+                return (d.N_report / 2);
             }) //圓圈大小
-            .style("fill", function (d, i) {return colors(i);})
+            .style("fill", function (d, i) {
+                return colors(i);
+            })
 
         //圓圈的名字//
-        node.append("title") 
-            .text(function (d) {return d.id;});
-        
+        node.append("title")
+            .text(function (d) {
+                return d.id;
+            });
+
         //圓圈的名字的位置//
         node.append("text")
             .attr("dy", -8)
             .attr("style", "fill: #ffffff")
-            .text(function (d) {return d.name;});
+            .text(function (d) {
+                return d.name;
+            });
 
         simulation
             .nodes(nodes)
@@ -68,30 +80,40 @@
 
     function ticked() {
         link
-            .attr("x1", function (d) {return d.source.x;})
-            .attr("y1", function (d) {return d.source.y;})
-            .attr("x2", function (d) {return d.target.x;})
-            .attr("y2", function (d) {return d.target.y;});
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
 
         node
-            .attr("transform", function (d) {return "translate(" + d.x + ", " + d.y + ")";});
+            .attr("transform", function (d) {
+                return "translate(" + d.x + ", " + d.y + ")";
+            });
 
-//        edgepaths.attr('d', function (d) {
-//            return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
-//        });
-//
-//        edgelabels.attr('transform', function (d) {
-//            if (d.target.x < d.source.x) {
-//                var bbox = this.getBBox();
-//
-//                rx = bbox.x + bbox.width / 2;
-//                ry = bbox.y + bbox.height / 2;
-//                return 'rotate(180 ' + rx + ' ' + ry + ')';
-//            }
-//            else {
-//                return 'rotate(0)';
-//            }
-//        });
+        //        edgepaths.attr('d', function (d) {
+        //            return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
+        //        });
+        //
+        //        edgelabels.attr('transform', function (d) {
+        //            if (d.target.x < d.source.x) {
+        //                var bbox = this.getBBox();
+        //
+        //                rx = bbox.x + bbox.width / 2;
+        //                ry = bbox.y + bbox.height / 2;
+        //                return 'rotate(180 ' + rx + ' ' + ry + ')';
+        //            }
+        //            else {
+        //                return 'rotate(0)';
+        //            }
+        //        });
     }
 
     function dragstarted(d) {
@@ -104,5 +126,3 @@
         d.fx = d3.event.x;
         d.fy = d3.event.y;
     }
-
-
